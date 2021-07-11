8ef000cbad73ebf8a02ee831e9a06301b08712b0
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-07-11 13:21:52 +0300

    Initial Commit

---
+++ AppMapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMapKit
+//
+//  Created by Alexander Sobolev on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
